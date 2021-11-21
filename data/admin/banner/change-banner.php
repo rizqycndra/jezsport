5407623ba7dafe9235aa5fc16760096c116b9e48
Rizqycndra <rzqcandra@gmail.com>
2021-11-21 14:12:19 +0700

    Merubah redirect change banner

--- data/admin/banner/change-banner.php
+++ data/admin/banner/change-banner.php
@@ -13,32 +13,35 @@ if(isset($_POST["submit"])) {
   if($check !== false) {
     $uploadOk = 1;
   } else {
-    header("Location: " . $_SERVER['HTTP_REFERER'] . "?q=error");
+    header("Location: ../../../admin/banner?q=error");
+    die;
     $uploadOk = 0;
   }
 }
 
 // Allow certain file formats
 if($imageFileType != "png") {
-  header("Location: " . $_SERVER['HTTP_REFERER'] . "?q=wrongformat");
+  header("Location: ../../../admin/banner?q=wrongformat");
+  die;
   $uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 3000000) {
-  header("Location: " . $_SERVER['HTTP_REFERER'] . "?q=toolarge");
+  header("Location: ../../../admin/banner?q=toolarge");
+  die;
   $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  header("Location: " . $_SERVER['HTTP_REFERER'] . "?q=error");
+  header("Location: ../../../admin/banner?q=error");
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    header("Location: " . $_SERVER['HTTP_REFERER'] . "?q=success");
+    header("Location: ../../../admin/banner?q=success");
   } else {
-    header("Location: " . $_SERVER['HTTP_REFERER'] . "?q=error");
+    header("Location: ../../../admin/banner?q=error");
   }
 }
 
